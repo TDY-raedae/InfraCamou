beccf51a3a32a85087a8c5fd3b6cc2b8d01a875d
TDY-raedae <81844139+TDY-raedae@users.noreply.github.com>
2024-10-16 19:41:50 +0800

    Delete InfraCamou directory

--- InfraCamou/proxy.py
+++
@@ -1,8 +0,0 @@
-import os
-import requests
-
-os.environ['http_proxy'] = 'http://proxy.com:port'
-os.environ['https_proxy'] = 'http://proxy.com:port'
-
-url = 'https://github.com/user/repo.git'
-response = requests.get(url)
